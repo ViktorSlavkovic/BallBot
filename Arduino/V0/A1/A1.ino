23b36465b88a91f171967b1b353cdab31f9a0b25
Viktor Slavkovic <viktor.slavkovic@gmail.com>
2017-12-04 22:04:14 +0100

    Adds communication between the arduinos.

--- Arduino/V0/A1/A1.ino
+++ Arduino/V0/A1/A1.ino
@@ -12,9 +12,38 @@ void setup() {
   pinMode(kBuzzPin, OUTPUT);
   digitalWrite(kBuzzPin, LOW);
 
+  delay (10000);
+
+  Serial.begin(115200);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void SendLower3b(uint8_t xl) {
+  uint8_t mask = 1;
+  for (int i = 0; i < 3; i++) {
+    digitalWrite(kSendDataPins[i], xl & mask);
+    mask <<= 1;
+  }
+  digitalWrite(kSendIntPin, HIGH);
+  delay(1);
+  digitalWrite(kSendIntPin, LOW);
+  delay(1);
+}
 
+void SendByte(uint8_t x) {
+  SendLower3b(x);
+  x >>= 3;
+  SendLower3b(x);
+  x >>= 3;
+  SendLower3b(x);
+}
+
+uint8_t i = 0;
+
+void loop() {
+//    SendByte(i++);
+//    delay(300);
+  if (Serial.available()) {
+    SendByte(Serial.read());
+    delay(10);
+  }
 }
--- Arduino/V0/A2/A2.ino
+++ Arduino/V0/A2/A2.ino
@@ -4,16 +4,69 @@ const int kDirPins[] = {11, 9, 7};
 const int kRecvDataPins[] = {3, 4, 5, 6};
 const int kRecvIntPin = 2;
 
-const int stepTime[] = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024};
+volatile int recv_state = 0; // 0 - clear, 1 - 3b, 2 - 6b
+volatile uint8_t recv_byte = 0x55;
+//volatile uint8_t recv_correct = 0;
+//volatile unsigned long t = millis();
+
+
+void RecvIntRoutine() {
+//  unsigned long t1 = millis();
+//  unsigned long dt = t1 - t;
+//  if (dt < 300) return;
+//  if (dt > 1000) recv_state = CLEAR; 
+  
+  //Serial.println("RecvIntRoutine()");
+  uint8_t x = 0;
+  for (int i = 0; i < 3; i++) {
+    uint8_t mask = digitalRead(kRecvDataPins[i]) << i;
+    x |= mask;
+  }
+
+  switch (recv_state) {
+    case 0: {
+      recv_byte = x;
+      recv_state++;
+      break;
+    }
+    case 1: {
+      recv_byte |= x << 3;
+      recv_state++;
+      break;
+    }
+    case 2: {
+      recv_byte |= x << 6;
+      recv_state = 0;
+      //Serial.print((int)recv_correct++); Serial.print("\t"); Serial.println((int)recv_byte);
+      Serial.write(recv_byte);
+      break;
+    }
+  }
+
+//  t = t1;
+}
 
 int dir = HIGH;
+const int stepTime[] = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024};
 
 void setup() {
+  Serial.begin(115200);
+  
   for (int i = 0; i < 3; i++) {
     pinMode(kStepPins[i], OUTPUT);
     pinMode(kDirPins[i], OUTPUT);
+    digitalWrite(kStepPins[i], dir);
     digitalWrite(kDirPins[i], dir);
   }
+
+  for (int i = 0; i < 4; i++) {
+    pinMode(kRecvDataPins[i], INPUT);
+  }
+  
+  pinMode(kRecvIntPin, INPUT);
+  delay(3000);
+  Serial.println("Attach");
+  attachInterrupt(digitalPinToInterrupt(kRecvIntPin), RecvIntRoutine, RISING);
 }
 
 void loop() {
@@ -42,7 +95,7 @@ void loop() {
 
   delay(1000);
 }
-
+//
 ///*  Simple Stepper Motor Control
 // *      
 // *  by Achim Pieters, www.studiopieters.nl
